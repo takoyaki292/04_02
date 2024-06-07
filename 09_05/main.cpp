1dbaac2747a491998552872cb55e59b14a6c2d0d
takoyaki292 <k023g1046@g.neec.ac.jp>
2024-06-07 15:28:58 +0900

    確認課題

--- 09_05/main.cpp
+++ 09_05/main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 //Type型自体を作る
-template<typename Type>
+template<typename Type1, typename Type2>
 
 
 class NumMin
@@ -8,23 +8,23 @@ class NumMin
 public:
 
 	//Type型のメンバ変数を作る
-	Type one;
-	Type two;
+	Type1 one;
+	Type2 two;
 	//コンストラクタ
-	NumMin(Type numOne, Type numTwo) :one(numOne), two(numTwo) {};
+	NumMin(Type1 numOne, Type2 numTwo) :one(numOne), two(numTwo) {};
 
 	//小さい値を返す関数
-	Type Min()
+	Type1 Min()
 	{
 		if (one < two)
 		{
 			//oneを呼び出す
-			return static_cast<Type>(one);
+			return static_cast<Type1>(one);
 		}
 		else
 		{
 			//twoを呼び出す
-			return static_cast<Type>(two);
+			return static_cast<Type1>(two);
 		}
 	}
 };
@@ -32,13 +32,19 @@ public:
 int main()
 {
 	//クラステンプレートを使って、初期化している
-	NumMin<int>b1(10, 20);
-	NumMin<float>b2(3.0, 2.0);
-	NumMin<double>b3(50.0, 20.0);
-
+	NumMin<int,int>b1(10, 20);
+	NumMin<float,float>b2(3.0, 2.0);
+	NumMin<double,double>b3(50.0, 20.0);
+	NumMin<int, float>b4(10, 2.0);
+	NumMin<int, double>b5(10, 20);
+	NumMin<float, double>b6(1.0, 2.0);
 	//出力する
 	printf("%d\n", b1.Min());
 	printf("%f\n", b2.Min());
 	printf("%lf\n", b3.Min());
+	printf("%d \n", b4.Min());
+	printf("%d\n", b5.Min());
+	printf("%lf\n", b6.Min());
+
 	return 0;
 }
